9a0e7eda942b4757c4d3493fd71d4aa37fa4b547
Harish-Naruto <harishnarote7@gmail.com>
2026-06-23 20:51:16 +0530

    refactor infrastructure to support containerized development with Docker Compose and hot-reloading

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -37,12 +37,6 @@ export default{
     JWT_SECRET: validateEnvVar('JWT_SECRET', process.env.JWT_SECRET),
     SALTING: validateEnvVar('SALTING', process.env.SALTING),
     API_URL: validateEnvVar('API_URL', process.env.API_URL),
-    GOOGLE_CLIENT_ID: validateEnvVar('GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID),
-    GOOGLE_CLIENT_SECRET: validateEnvVar('GOOGLE_CLIENT_SECRET', process.env.GOOGLE_CLIENT_SECRET),
-    GOOGLE_CALLBACK_URL: validateEnvVar('GOOGLE_CALLBACK_URL', process.env.GOOGLE_CALLBACK_URL),
-    GITHUB_CLIENT_ID: validateEnvVar('GITHUB_CLIENT_ID', process.env.GITHUB_CLIENT_ID),
-    GITHUB_CLIENT_SECRET: validateEnvVar('GITHUB_CLIENT_SECRET', process.env.GITHUB_CLIENT_SECRET),
-    GITHUB_CALLBACK_URL: validateEnvVar('GITHUB_CALLBACK_URL', process.env.GITHUB_CALLBACK_URL),
     EMAIL_ID: validateEnvVar('EMAIL_ID', process.env.EMAIL_ID),
     RESEND_API_KEY: validateEnvVar('RESEND_API_KEY', process.env.RESEND_API_KEY),
 }
